ff16a5c64af0c174ee1c8032583c791aea5c6804
EPushkarev <johnny8921@gmail.com>
2024-07-19 15:48:42 +0300

    init opentherm module

--- ports/esp32/boards/ESP32_S3_WROOM_1/mpconfigboard.cmake
+++ ports/esp32/boards/ESP32_S3_WROOM_1/mpconfigboard.cmake
@@ -1,17 +1,18 @@
 set(IDF_TARGET esp32s3)
 
 set(SDKCONFIG_DEFAULTS
-    boards/sdkconfig.base
-    boards/sdkconfig.usb
-    boards/sdkconfig.ble
-    ${MICROPY_BOARD_DIR}/sdkconfig.board
+        boards/sdkconfig.base
+        boards/sdkconfig.usb
+        boards/sdkconfig.ble
+        ${MICROPY_BOARD_DIR}/sdkconfig.board
 )
 
 set(MICROPY_SOURCE_BOARD
-     ${MICROPY_BOARD_DIR}/machine_cdc.c
-#     ${MICROPY_BOARD_DIR}/machine_can.c
+        ${MICROPY_BOARD_DIR}/machine_cdc.c
+        ${MICROPY_BOARD_DIR}/opentherm.c
+        #     ${MICROPY_BOARD_DIR}/machine_can.c
 )
 
 set(MICROPY_FROZEN_MANIFEST
-    ${MICROPY_BOARD_DIR}/manifest.py
+        ${MICROPY_BOARD_DIR}/manifest.py
 )
