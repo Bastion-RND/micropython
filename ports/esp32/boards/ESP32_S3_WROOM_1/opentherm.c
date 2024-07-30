0481736f7d14195588134b77030c817bf2e9eaa8
EPushkarev <johnny8921@gmail.com>
2024-07-30 17:12:01 +0300

    EOD

--- ports/esp32/boards/ESP32_S3_WROOM_1/opentherm.c
+++ ports/esp32/boards/ESP32_S3_WROOM_1/opentherm.c
@@ -5,14 +5,19 @@
 #include "opentherm.h"
 #include "string.h"
 
+#define TIMER_DIVIDER  8
+#define TIMER_FLAGS    0
+
 // Base data struct storage
 const mp_obj_type_t opentherm_type;
 static uint8_t _rx_buf[128];
 static opentherm_obj_t opentherm_obj = {
         .base = {&opentherm_type},
+        .state = OT_NOT_INITIALIZED,
         .in = -1,
         .out = -1,
-        .debug = 6,
+        .isSlave = false,
+        .debug = 41,
         .timeout = 100,
         .invert = 0,
         .rx_buf = {
@@ -26,37 +31,10 @@ static opentherm_obj_t opentherm_obj = {
 
 static opentherm_obj_t *instance = &opentherm_obj;
 
-
-static OPENTHERM_Data encodeData;
-static OPENTHERM_Data decodeData;
-
-static uint8_t virtTact = 1;
-
-
-static OPENTHERM_DecodeEdge curEdge = NONE;
-static OPENTHERM_DecodeEdge prevEdge = NONE;
-
-static OPENTHERM_DecodeState decodeState = NOT_SYNC;
-
-static uint16_t encodeTimerCnt = 0;
-static uint16_t decodeTimerCnt = 0;
-
 static size_t write_to_buf(uint8_t *p, size_t len);
 
 
 //manchester Code functions
-void OPENTHERM_DataReadyCallback() {
-    mp_printf(&mp_plat_print, "data recv:[");
-    for (size_t i = 0; i < decodeData.bytesNum; i++) {
-        mp_printf(&mp_plat_print, "%X,", decodeData.data[i]);
-    }
-    mp_printf(&mp_plat_print, "]\n");
-
-    write_to_buf(decodeData.data, decodeData.bytesNum);
-
-    return;
-}
-
 static void set_output(uint8_t state) {
     gpio_set_level(instance->out, state);
 }
@@ -66,205 +44,128 @@ static bool get_input() {
     return (bool) gpio_get_level(instance->in);
 }
 
+#define INACTIVE    0
+#define ACTIVE      1
 
-/*----------------------------------------------------------------------------*/
-static uint8_t get_data_bit(OPENTHERM_Data *OPENTHERMData) {
-    uint8_t curByte = OPENTHERMData->data[OPENTHERMData->byteIdx];
-    uint8_t curBitIdx = OPENTHERMData->bitIdx;
-    return (curByte >> curBitIdx) & 0x01;
+void sendBit(bool high) {
+    set_output(high ? ACTIVE : INACTIVE);
+    mp_hal_delay_us(500);
+    set_output(high ? INACTIVE : ACTIVE);
+    mp_hal_delay_us(500);
 }
 
-
-/*----------------------------------------------------------------------------*/
-static void set_data_bit(OPENTHERM_Data *OPENTHERMData, uint8_t bit) {
-    uint8_t curByteIdx = OPENTHERMData->byteIdx;
-    uint8_t curBitIdx = OPENTHERMData->bitIdx;
-
-    if (bit == 1) {
-        OPENTHERMData->data[curByteIdx] |= (1 << curBitIdx);
-    }
+static bool isReady()
+{
+    return instance->state == OT_READY;
 }
 
-void timer_encode_callback() {
-    if ((encodeTimerCnt == (OPENTHERM_ENCODE_TIMER_MAX / 2)) ||
-        (encodeTimerCnt == OPENTHERM_ENCODE_TIMER_MAX)) {
-        uint8_t curCodeBit = get_data_bit(&encodeData);
-        uint8_t curOutputBit = curCodeBit ^ virtTact;
-        set_output(curOutputBit);
-        virtTact ^= 0x01;
-    }
-
-    if (encodeTimerCnt == OPENTHERM_ENCODE_TIMER_MAX) {
-        encodeData.bitIdx++;
-
-        if (encodeData.bitIdx == (OPENTHERM_BITS_IN_BYTE_NUM)) {
-            encodeData.bitIdx = 0;
-
-            encodeData.byteIdx++;
-            if (encodeData.byteIdx == encodeData.bytesNum) {
-                encodeData.active = 0;
-            }
-        }
-
-        encodeTimerCnt = 0;
-    }
-
-    encodeTimerCnt++;
-}
-
-void timer_decode_callback() {
-    if (decodeState == DATA_SYNC) {
-        if (decodeTimerCnt >= (3 * OPENTHERM_DECODE_TIMER_MAX)) {
-            decodeTimerCnt = 0;
-
-            decodeData.active = 0;
-            curEdge = NONE;
-            prevEdge = NONE;
-
-            decodeState = DATA_READY;
-
-            // Data is ready
-            OPENTHERM_DataReadyCallback();
-        }
-    }
-
-    decodeTimerCnt++;
-}
-
-// Timer callback
-
-void timer_callback() {
-    // Encoding process
-    if (encodeData.active) {
-        timer_encode_callback();
-    }
-
-    // Decoding process
-    if (decodeData.active) {
-        timer_decode_callback();
-    }
-}
-
-
-//
-// all code executed in ISR must be in IRAM, and any const data must be in DRAM
-static void opentherm_irq_handler(void *arg) {
-    // FIXME: DELETE DEBUG PIN AFTER DEBUG!!!
+static void handleInterrupt()
+{
     gpio_set_level(instance->debug, gpio_get_level(instance->in));
-
-    curEdge = get_input() ? FALLING_EDGE : RAISING_EDGE;
-
-    switch (decodeState) {
-        case NOT_SYNC:
-            if (!decodeData.active) {
-                decodeData.active = true;
-                decodeTimerCnt = 0;
-            } else {
-                if (((curEdge == FALLING_EDGE) && (prevEdge == RAISING_EDGE)) ||
-                    ((curEdge == RAISING_EDGE) && (prevEdge == FALLING_EDGE))) {
-                    if (decodeTimerCnt >= OPENTHERM_DECODE_TIMER_THRESHOLD) {
-                        if (curEdge == FALLING_EDGE) {
-                            decodeData.bitStream = 0x4000;
-                            decodeData.bitStream >>= 1;
-                        } else {
-                            decodeData.bitStream = 0x8000;
-                            decodeData.bitStream >>= 1;
-                        }
-                        for (uint8_t i = 0; i < OPENTHERM_BYTES_NUM; i++) {
-                            decodeData.data[i] = 0x00;
-                        }
-                        decodeState = BIT_SYNC;
-                    }
-                    decodeTimerCnt = 0;
+    if (isReady())
+    {
+        if (get_input())
+        {
+            instance->state = OT_RESPONSE_WAITING;
+        }
+        else
+        {
+            return;
+        }
+    }
+    unsigned long newTs = mp_hal_ticks_cpu();
+    switch (instance->state) {
+        case OT_RESPONSE_WAITING:
+            if (get_input())
+            {
+                instance->state = OT_RESPONSE_START_BIT;
+                instance->responseTimestamp = newTs;
+            }
+            else
+            {
+                instance->state = OT_RESPONSE_INVALID;
+                instance->responseTimestamp = newTs;
+            }
+            break;
+        case OT_RESPONSE_START_BIT:
+            if ((newTs - instance->responseTimestamp < 750) && !get_input())
+            {
+                instance->state = OT_RESPONSE_RECEIVING;
+                instance->responseTimestamp = newTs;
+                instance->responseBitIndex = 0;
+            }
+            else
+            {
+                instance->state = OT_RESPONSE_INVALID;
+                instance->responseTimestamp = newTs;
+            }
+            break;
+        case OT_RESPONSE_RECEIVING:
+            if ((newTs - instance->responseTimestamp) > 750)
+            {
+                if (instance->responseBitIndex < 32)
+                {
+                    instance->response = (instance->response << 1) | !get_input();
+                    instance->responseTimestamp = newTs;
+                    instance->responseBitIndex++;
+                }
+                else
+                { // stop bit
+                    instance->state = OT_READY;
+                    instance->response = 0;
+                    instance->responseBitIndex = 0;
+                    instance->responseTimestamp = newTs;
+                    write_to_buf((uint8_t *)instance->response, instance->responseBitIndex/8);
                 }
             }
             break;
-
-        case BIT_SYNC:
-            if (decodeTimerCnt < OPENTHERM_DECODE_TIMER_THRESHOLD) {
-                break;
-            }
-            if (curEdge == RAISING_EDGE) {
-                decodeData.bitStream |= 0x8000;
-            }
-
-            if (decodeData.bitStream == OPENTHERM_SYNC_FIELD) {
-                decodeState = DATA_SYNC;
-                decodeData.data[0] = decodeData.bitStream & 0xFF;
-                decodeData.data[1] = (decodeData.bitStream & 0xFF00) >> 8;
-                decodeData.bitIdx = 0;
-                decodeData.byteIdx = OPENTHERM_SYNC_BYTES_NUM;
-                decodeData.bytesNum = OPENTHERM_DATA_BYTES_NUM + OPENTHERM_SYNC_BYTES_NUM;
-            } else {
-                decodeData.bitStream >>= 1;
-            }
-            decodeTimerCnt = 0;
-            break;
-
-        case DATA_SYNC:
-            if (decodeTimerCnt >= OPENTHERM_DECODE_TIMER_THRESHOLD) {
-                if (curEdge == RAISING_EDGE) {
-                    set_data_bit(&decodeData, 1);
-                }
-
-                decodeData.bitIdx++;
-
-                if (decodeData.bitIdx == (OPENTHERM_BITS_IN_BYTE_NUM)) {
-                    decodeData.bitIdx = 0;
-
-                    decodeData.byteIdx++;
-                    if (decodeData.byteIdx == decodeData.bytesNum) {
-                        decodeData.active = 0;
-                        curEdge = NONE;
-                        prevEdge = NONE;
-                        decodeState = DATA_READY;
-                        // Data is ready
-                        OPENTHERM_DataReadyCallback();
-                    }
-                }
-                decodeTimerCnt = 0;
-            }
-            break;
-
-        case DATA_READY:
-            break;
-
         default:
             break;
     }
 
-    prevEdge = curEdge;
-    return;
 }
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_log.h"
+#include "esp_check.h"
 
-void OPENTHERM_Encode(uint8_t *data, uint8_t size) {
-    encodeData.bitIdx = 0;
-    encodeData.byteIdx = 0;
-
-    if (size > OPENTHERM_DATA_BYTES_NUM) {
-        encodeData.bytesNum = OPENTHERM_DATA_BYTES_NUM + OPENTHERM_SYNC_BYTES_NUM;
-    } else {
-        encodeData.bytesNum = size + OPENTHERM_SYNC_BYTES_NUM;
+const static char *TAG = "tusb_tsk";
+static TaskHandle_t s_ot_tskh;
+/**
+ * @brief This top level thread processes all usb events and invokes callbacks
+ */
+static void ot_device_task(void *arg)
+{
+    mp_print_str(&mp_plat_print, "OT task started");
+    while (1) { // RTOS forever loop
+        if(mp_hal_ticks_cpu() - instance->responseTimestamp >= 40000){
+            instance->response = 0;
+            instance->responseBitIndex = 0;
+            instance->state = OT_READY;
+            instance->responseTimestamp = mp_hal_ticks_cpu();
+//            write_to_buf((uint8_t *)instance->response, instance->responseBitIndex/8);
+        }
     }
-
-    memcpy(&encodeData.data[OPENTHERM_SYNC_BYTES_NUM], data, encodeData.bytesNum - OPENTHERM_SYNC_BYTES_NUM);
-    encodeData.data[0] = OPENTHERM_SYNC_FIELD & 0xFF;
-    encodeData.data[1] = (OPENTHERM_SYNC_FIELD & 0xFF00) >> 8;
-
-    encodeTimerCnt = 0;
-    virtTact = 1;
-    encodeData.active = 1;
 }
 
-
-/*----------------------------------------------------------------------------*/
-void OPENTHERM_DecodeReset() {
-    decodeState = NOT_SYNC;
+esp_err_t ot_run_task(void)
+{
+    // This function is not garanteed to be thread safe, if invoked multiple times without calling `tusb_stop_task`, will cause memory leak
+    // doing a sanity check anyway
+    ESP_RETURN_ON_FALSE(!s_ot_tskh, ESP_ERR_INVALID_STATE, TAG, "OT main task already started");
+    // Create a task for tinyusb device stack:
+    xTaskCreate(ot_device_task, "OpenTherm", 4096, NULL, 5, &s_ot_tskh);
+    ESP_RETURN_ON_FALSE(s_ot_tskh, ESP_FAIL, TAG, "create OT main task failed");
+    return ESP_OK;
 }
 
-static void mp_opentherm_timer_config() {
-
+esp_err_t ot_stop_task(void)
+{
+    ESP_RETURN_ON_FALSE(s_ot_tskh, ESP_ERR_INVALID_STATE, TAG, "OT main task not started yet");
+    vTaskDelete(s_ot_tskh);
+    s_ot_tskh = NULL;
+    return ESP_OK;
 }
 
 static void mp_opentherm_gpio_irq_config() {
@@ -272,9 +173,9 @@ static void mp_opentherm_gpio_irq_config() {
     if (self == mp_const_none)
         return;
     gpio_isr_handler_remove(self->in);
-    gpio_set_intr_type(self->in, GPIO_INTR_ANYEDGE);
-    gpio_isr_handler_add(self->in, opentherm_irq_handler, (void *) self);
-    mp_printf(&mp_plat_print, "Done!!!\n");
+    gpio_set_intr_type(self->in, GPIO_INTR_POSEDGE | GPIO_INTR_NEGEDGE);
+    gpio_isr_handler_add(self->in, handleInterrupt, (void *) self);
+    mp_printf(&mp_plat_print, "IRQ config Done!!!\n");
 }
 
 static void mp_opentherm_print(const mp_print_t *print, mp_obj_t self_in, mp_print_kind_t kind) {
@@ -291,15 +192,19 @@ static void mp_opentherm_print(const mp_print_t *print, mp_obj_t self_in, mp_pri
 //static void mp_opentherm_deinit(opentherm_hw_obj_t *self){
 //    return self;
 //}
-
+//
+// from machine import Pin
+// import opentherm
+//d = Pin(41, Pin.OUT)
+//o = opentherm.OpenTherm(Pin(7, Pin.IN), Pin(15,Pin.OUT))
 // opentherm.init(in, out, timeout, invert)
 static mp_obj_t opentherm_make_new(const mp_obj_type_t *type, size_t n_args, size_t n_kw, const mp_obj_t *all_args) {
     enum {
         ARG_in, ARG_out, ARG_timeout, ARG_invert
     };
     static const mp_arg_t allowed_args[] = {
-            {MP_QSTR_in,      MP_ARG_INT,                  {.u_int = 0}},
-            {MP_QSTR_out,     MP_ARG_INT,                  {.u_int = 0}},
+            {MP_QSTR_in,      MP_ARG_OBJ,                  {.u_obj = mp_const_none}},
+            {MP_QSTR_out,     MP_ARG_OBJ,                  {.u_obj = mp_const_none}},
             {MP_QSTR_timeout, MP_ARG_KW_ONLY | MP_ARG_INT, {.u_int = -1}},
             {MP_QSTR_invert,  MP_ARG_KW_ONLY | MP_ARG_INT, {.u_int = -1}},
     };
@@ -311,22 +216,19 @@ static mp_obj_t opentherm_make_new(const mp_obj_type_t *type, size_t n_args, siz
     // create instance
     opentherm_obj_t *self = MP_OBJ_TO_PTR(instance);
     // parse input agguments
-    if (args[ARG_in].u_int > 0) {
-        self->in = args[ARG_in].u_int;
+    if (args[ARG_in].u_obj != mp_const_none) {
+        self->in = machine_pin_get_id(args[ARG_in].u_obj);
     } else {
         mp_raise_ValueError(MP_ERROR_TEXT("invalid IN Pin"));
     }
-    if (args[ARG_out].u_int > 0) {
-        self->out = args[ARG_out].u_int;
+    if (args[ARG_in].u_obj != mp_const_none) {
+        self->out = machine_pin_get_id(args[ARG_out].u_obj);
     } else {
         mp_raise_ValueError(MP_ERROR_TEXT("invalid OUT Pin"));
     }
     self->timeout = (args[ARG_timeout].u_int > 0) ? args[ARG_timeout].u_int : OT_DEFAULT_TIMEOUT;
     self->invert = (args[ARG_invert].u_int >= 0) ? args[ARG_invert].u_int : OT_DEFAULT_INVERSION;
-
-    gpio_set_direction(self->in, GPIO_MODE_INPUT);
-    gpio_set_direction(self->out, GPIO_MODE_OUTPUT);
-    gpio_set_direction(self->debug, GPIO_MODE_OUTPUT);
+//    ot_run_task();
     mp_opentherm_gpio_irq_config();
     return MP_OBJ_FROM_PTR(self);
 }
@@ -348,14 +250,14 @@ static mp_obj_t opentherm_write(mp_obj_t self_in, mp_obj_t data) {
     if (mp_obj_is_type(data, &mp_type_bytes)) {
         mp_buffer_info_t bufinfo;
         mp_get_buffer_raise(data, &bufinfo, MP_BUFFER_READ);
-        OPENTHERM_Encode(bufinfo.buf, bufinfo.len);
+//        OPENTHERM_Encode(bufinfo.buf, bufinfo.len);
         return mp_obj_new_int(bufinfo.len);
     } else if (mp_obj_is_type(data, &mp_type_list) ||
                mp_obj_is_type(data, &mp_type_tuple)) {
         size_t len = 0;
         mp_obj_t *target_array_ptr = NULL;
         mp_obj_get_array(data, &len, &target_array_ptr);
-        OPENTHERM_Encode(MP_OBJ_TO_PTR(target_array_ptr), len);
+//        OPENTHERM_Encode(MP_OBJ_TO_PTR(target_array_ptr), len);
         return mp_obj_new_int(len);
 
     } else {
--- ports/esp32/boards/ESP32_S3_WROOM_1/opentherm.h
+++ ports/esp32/boards/ESP32_S3_WROOM_1/opentherm.h
@@ -16,73 +16,45 @@
 #include "py/obj.h"
 // Used to get the time in the Timer class example.
 #include "py/mphal.h"
+#include "hal/timer_hal.h"
+#include "hal/timer_ll.h"
+#include "soc/timer_periph.h"
 
-/* Declarations and definitions ----------------------------------------------*/
-
-#define OPENTHERM_ENCODE_TIMER_PERIOD_US                            10
-#define OPENTHERM_BIT_TIME_US                                       500
-
-#define OPENTHERM_BYTES_NUM                                         4
-#define OPENTHERM_SYNC_BYTES_NUM                                    2
-#define OPENTHERM_DATA_BYTES_NUM                                    OPENTHERM_BYTES_NUM - OPENTHERM_SYNC_BYTES_NUM
-
-#define OPENTHERM_BITS_IN_BYTE_NUM                                  8
-
-#define OPENTHERM_SYNC_FIELD                                        0xAA55
-
-#define OPENTHERM_DECODE_TIMER_PERIOD_US                            10
-
-#define OPENTHERM_ENCODE_TIMER_MAX                                  OPENTHERM_BIT_TIME_US / OPENTHERM_ENCODE_TIMER_PERIOD_US
-
-#define OPENTHERM_DECODE_TIMER_MAX                                  OPENTHERM_BIT_TIME_US / OPENTHERM_DECODE_TIMER_PERIOD_US
-#define OPENTHERM_DECODE_TIMER_THRESHOLD                            OPENTHERM_DECODE_TIMER_MAX * 3 / 4
-
-
-
-typedef enum
-{
-    NOT_SYNC,
-    BIT_SYNC,
-    DATA_SYNC,
-    DATA_READY
-} OPENTHERM_DecodeState;
-
-typedef enum
-{
-    NONE,
-    RAISING_EDGE,
-    FALLING_EDGE
-} OPENTHERM_DecodeEdge;
-
-typedef struct OPENTHERM_Data
-{
-    uint8_t data[OPENTHERM_BYTES_NUM];
-    uint16_t bitStream;
-    uint16_t byteIdx;
-    uint16_t bytesNum;
-    uint8_t bitIdx;
-    bool active;
-} OPENTHERM_Data;
-
-
+typedef enum {
+    OT_NOT_INITIALIZED,
+    OT_READY,
+    OT_DELAY,
+    OT_REQUEST_SENDING,
+    OT_RESPONSE_WAITING,
+    OT_RESPONSE_START_BIT,
+    OT_RESPONSE_RECEIVING,
+    OT_RESPONSE_READY,
+    OT_RESPONSE_INVALID
+} OpenThermStatus;
 
 #define OT_DEFAULT_TIMEOUT 100
 #define OT_DEFAULT_INVERSION 0
 
 typedef struct _opentherm_obj_t {
     mp_obj_base_t base;
+    OpenThermStatus state;
     gpio_num_t in;
     gpio_num_t out;
+    mp_int_t isSlave;
     gpio_num_t debug;
     mp_int_t invert;
     mp_int_t timeout;
+    uint32_t responseTimestamp;
+    uint8_t responseBitIndex;
+    uint32_t response;
     ringbuf_t rx_buf;
 } opentherm_obj_t;
 
 
 /* Functions -----------------------------------------------------------------*/
 
-extern void OPENTHERM_Encode(uint8_t* data, uint8_t size);
+extern void OPENTHERM_Encode(uint8_t *data, uint8_t size);
+
 extern void OPENTHERM_DecodeReset();
 
 
